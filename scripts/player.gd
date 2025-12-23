4389955c1e51e4c8cf3ecb8dfd9d6058ee24d7b2
Lenon Assalin <lenonassalin@gmail.com>
2025-12-23 16:58:50 -0300

    add jump

--- scripts/player.gd
+++ scripts/player.gd
@@ -23,12 +23,15 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		
-	if direction > 0:
-		anim.flip_h = false
-		anim.play("walk")
-	elif direction < 0:
-		anim.flip_h = true
-		anim.play("walk")
-	else: anim.play("idle")
+	if is_on_floor():
+		if direction > 0:
+			anim.flip_h = false
+			anim.play("walk")
+		elif direction < 0:
+			anim.flip_h = true
+			anim.play("walk")
+		else: anim.play("idle")
+	else:
+		anim.play("jump")
 
 	move_and_slide()
