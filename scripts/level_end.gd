11e9f3bbf78c4e3420a667506184a61f47c01b64
Lenon Assalin <lenon.assalin@gmail.com>
2026-01-04 21:04:13 -0300

    feat: Added phase switching function.

--- scripts/level_end.gd
+++ scripts/level_end.gd
@@ -1,6 +1,10 @@
 extends Area2D
 
+@export var next_level = ""
+
+func _on_body_entered(_body: Node2D) -> void:
+	call_deferred("load_next_scene")
 
 
-func _on_body_entered(body: Node2D) -> void:
-	print("Player passou")
+func load_next_scene():
+	get_tree().change_scene_to_file("res://scene/" + next_level + ".tscn")
