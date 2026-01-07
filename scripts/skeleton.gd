d81a7e5852c10ddcc7da810334abb378e306cced
Lenon Assalin <lenon.assalin@gmail.com>
2026-01-06 23:12:52 -0300

    feat: Added projectiles and animations for enemies launching projectiles.

--- scripts/skeleton.gd
+++ scripts/skeleton.gd
@@ -2,20 +2,24 @@ extends CharacterBody2D
 
 enum SkeletonState {
 	walk,
+	attack,
 	hurt
 }
+const SPINNING_BONE = preload("uid://0ualwmc7fww4")
 
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 @onready var hitbox: Area2D = $Hitbox
 @onready var wall_detector: RayCast2D = $WallDetector
 @onready var ground_detector: RayCast2D = $GroundDetector
+@onready var player_detector: RayCast2D = $PlayerDetector
+@onready var bone_start_position: Node2D = $BoneStartPosition
 
 const SPEED = 10.0
 const JUMP_VELOCITY = -400.0
 
 var direction = 1
 var status: SkeletonState
-
+var can_throw = true
 
 func _ready() -> void:
 	go_to_walk_state()
@@ -30,6 +34,8 @@ func _physics_process(delta: float) -> void:
 	match status:
 		SkeletonState.walk:
 			walk_state(delta)
+		SkeletonState.attack:
+			attack_state(delta)
 		SkeletonState.hurt:
 			hurt_state(delta)
 
@@ -39,6 +45,12 @@ func go_to_walk_state():
 	status = SkeletonState.walk
 	anim.play("walk")
 	
+func go_to_attack_state():
+	status = SkeletonState.attack
+	anim.play("attack")
+	velocity = Vector2.ZERO
+	can_throw = true
+		
 func go_to_hurt_state():
 	status = SkeletonState.hurt
 	anim.play("hurt")
@@ -55,9 +67,30 @@ func walk_state(_delta):
 	if !ground_detector.is_colliding():
 		scale.x *= -1
 		direction *= -1
+		
+	if player_detector.is_colliding():
+		go_to_attack_state()
+		return
+		
+func attack_state(_delta):
+	if anim.frame == 2 && can_throw:
+		throw_bone()
+		can_throw = false
 	
 func hurt_state(_delta):
 	pass
 	
 func take_damege():
 	go_to_hurt_state()
+
+func throw_bone():
+	var new_bone = SPINNING_BONE.instantiate()
+	add_sibling(new_bone)
+	new_bone.position = bone_start_position.global_position
+	new_bone.set_direction(self.direction)
+	
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if anim.animation == "attack":
+		go_to_walk_state()
+		return
--- scripts/spinning_bone.gd
+++ scripts/spinning_bone.gd
@@ -1,8 +1,15 @@
 extends Area2D
 
+@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
+
 var speed = 60
 var direction = 1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	position.x += speed * delta * direction
+	
+func set_direction(dir):
+	self.direction = dir
+	anim.flip_h = direction < 0
+	
